0e4a049befa7ef5dc551b6f468b9c004cd9c9e95
Johan van Eck <johveck@gmail.com>
2020-06-03 15:31:24 +0300

    Detect platform

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-./node_modules/.bin/nativefier --darwin-dark-mode-support --clear-cache --icon giosg_ball_1024.png --platform macos --name "Smackbar" https://service.giosg.com/bar
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  OS="osx"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  OS="linux"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW" ]; then
+  OS="windows"
+else
+  echo "your platform is not recognized"
+  exit 1
+fi
+
+./node_modules/.bin/nativefier --darwin-dark-mode-support --clear-cache --icon giosg_ball_1024.png --platform $OS --name "Smackbar" https://service.giosg.com/bar
